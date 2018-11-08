9eafe0c6118bd0c5222c763c9ee50959ee04576e
Andrés Liu <adavidliuf@gmail.com>
2018-11-08 00:12:22 -0500

    Finished base layout of editor page

--- js/modules/editor.js
+++ js/modules/editor.js
@@ -1,6 +1,18 @@
 class Editor {
     constructor() {
-        $(".editor__status-chart").circleProgress({
+        this.selectors = {
+            statusChart: "editor__status-chart",
+            sortableSelector: "[data-sortable]",
+            educationEditor: "editor__education-wysiwyg"
+        }
+
+        this.$DOM = {
+            statusChart: $("." + this.selectors.statusChart),
+            sortableSelector: $(this.selectors.sortableSelector),
+            educationEditor: $("." + this.selectors.educationEditor)
+        }
+        
+        this.$DOM.statusChart.circleProgress({
             value: .65,
             size: 1000,
             fill: "#61abfd",
@@ -9,14 +21,18 @@ class Editor {
             lineCap: "round"
         });
 
-        const $sortableSelector = $("[data-sortable]");
-        $sortableSelector.each(function(i) {
+        this.$DOM.sortableSelector.each(function(i) {
             const $this = $(this);
             const sortable = Sortable.create($this[0], {
                 animation: 150,
                 handle: ".fa-bars",
                 forceFallback: true
             });
+        });
+
+        this.$DOM.educationEditor.trumbowyg({
+            resetCss: true,
+            btns: [["fontfamily"], ["strong", "em", "underline", "del", "justifyLeft", "justifyCenter", "justifyRight", "justifyFull"]]
         });
     }
 }
